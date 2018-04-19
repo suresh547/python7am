bc8d998fe886a2e482d1a53c86a64e6516ab4c3c
DevOps Keshav <30571617+devopskeshav@users.noreply.github.com>
2018-04-19 07:59:17 +0530

    MyFirstPythonFile

---
+++ firstPythonProg.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Comments : 1. Single Comments   : #, '', "", ''' ''' & ''' '''
+#		   2. Multiline Comments: ''' ''', & """ """
+
+
+'Create varibles'
+
+fname = 'Gudio'
+mname = "Van"
+lname = "Rossum"
+
+"Accessing the varibles"
+
+print(fname)
+print("")
+print(mname)
+print("")
+print(lname)
+print("")
